90b5e48c7e83632e2312f311905a3348290e8a19
Sadam Hussain <d_shussain@universalweather.net>
2024-08-30 13:27:51 -0400

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/shared/Provider";
@@ -43,6 +44,7 @@ export default function RootLayout({
           </main>
         </ThemeProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
